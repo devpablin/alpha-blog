428a1562c980f5165c27fb083e472aed8d6a44ea
Pablo <pablingonzales@gmail.com>
2016-04-26 10:09:17 -0400

    sign up users from UI

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Bienvenido a Deportes 3600 #{@user.username}"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
+end
